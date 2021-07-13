2b3f87919c1958ef017575a1338d3e7beac88e5e
L. Nicole Bent <l.nicole.bent@gmail.com>
2021-07-13 16:58:21 -0300

    Last commit before leaving for work. Attempting to change status column to drop down menu.

--- Project/Models/SeedData.cs
+++ Project/Models/SeedData.cs
@@ -10,51 +10,51 @@ namespace Project.Models
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            using (var context = new ProjectContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<ProjectContext>>()))
-            {
-                // Similar setup found in tutorial, edited to match requested info
-                if (context.Book.Any())
-                {
-                    return;   // DB has been seeded
-                }
+            //using (var context = new ProjectContext(
+            //    serviceProvider.GetRequiredService<
+            //        DbContextOptions<ProjectContext>>()))
+            //{
+            //    // Similar setup found in tutorial, edited to match requested info
+            //    if (context.Book.Any())
+            //    {
+            //        return;   // DB has been seeded
+            //    }
 
-                context.Book.AddRange(
-                    new Book
-                    {
-                        Title = "Fahrenheit 451",
-                        PublishDate = DateTime.Parse("1953-10-19"),
-                        Author = "Ray Bradbury",
-                        Status = ""
-                    },
+            //    context.Book.AddRange(
+            //        new Book
+            //        {
+            //            Title = "Fahrenheit 451",
+            //            PublishDate = DateTime.Parse("1953-10-19"),
+            //            Author = "Ray Bradbury",
+            //            Status = 
+            //        },
 
-                    new Book
-                    {
-                        Title = "The Hobbit",
-                        PublishDate = DateTime.Parse("1937-9-12"),
-                        Author = "J. R. R. Tolkien",
-                        Status = ""
-                    },
+            //        new Book
+            //        {
+            //            Title = "The Hobbit",
+            //            PublishDate = DateTime.Parse("1937-9-12"),
+            //            Author = "J. R. R. Tolkien",
+            //            Status =
+            //        },
 
-                    new Book
-                    {
-                        Title = "One Flew Over the Cuckoo's Nest",
-                        PublishDate = DateTime.Parse("1962-2-1"),
-                        Author = "Ken Kessey",
-                        Status = ""
-                    },
+            //        new Book
+            //        {
+            //            Title = "One Flew Over the Cuckoo's Nest",
+            //            PublishDate = DateTime.Parse("1962-2-1"),
+            //            Author = "Ken Kessey",
+            //            Status =
+            //        },
 
-                    new Book
-                    {
-                        Title = "Little Women",
-                        PublishDate = DateTime.Parse("1869-12-2"),
-                        Author = "Louisa May Alcott",
-                        Status = ""
-                    }
-                );
-                context.SaveChanges();
-            }
+            //        new Book
+            //        {
+            //            Title = "Little Women",
+            //            PublishDate = DateTime.Parse("1869-12-2"),
+            //            Author = "Louisa May Alcott",
+            //            Status =
+            //        }
+            //    );
+            //    context.SaveChanges();
+            //}
         }
     }
 }
