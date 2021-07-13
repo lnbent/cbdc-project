a5dca362b368ce5d6349464cd0e3a90d8942d3dc
L. Nicole Bent <l.nicole.bent@gmail.com>
2021-07-13 14:51:32 -0300

    Added SeedData class.

---
+++ Project/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Data;
+using System;
+using System.Linq;
+
+namespace Project.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProjectContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ProjectContext>>()))
+            {
+                // Similar setup found in tutorial, edited to match requested info
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                    new Book
+                    {
+                        Title = "Fahrenheit 451",
+                        PublishDate = DateTime.Parse("1953-19-10"),
+                        Author = "Ray Bradbury"
+                    },
+
+                    new Book
+                    {
+                        Title = "The Hobbit",
+                        PublishDate = DateTime.Parse("1937-21-09"),
+                        Author = "J. R. R. Tolkien"
+                    },
+
+                    new Book
+                    {
+                        Title = "One Flew Over the Cuckoo's Nest",
+                        PublishDate = DateTime.Parse("1962-1-02"),
+                        Author = "Ken Kessey"
+                    },
+
+                    new Book
+                    {
+                        Title = "Little Women",
+                        PublishDate = DateTime.Parse("1869-2-12"),
+                        Author = "Louisa May Alcott"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Project.Data;
+using Project.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Project
 {
@@ -13,7 +12,24 @@ namespace Project
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
