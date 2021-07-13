308a6c281701737e0a38ffaf4fc74ce1b1fc1f58
L. Nicole Bent <l.nicole.bent@gmail.com>
2021-07-13 14:05:37 -0300

    Fixed majour errors caused by previous merge

---
+++ Project/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+    }
+}
